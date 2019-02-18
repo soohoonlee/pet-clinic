66e5d22495fc2e77d66139e7f642c574e8717aad
Soohoon Lee <shlee@nvista.co.kr>
2019-02-18 11:28:59 +0900

    Adding POJOs. Closes #2.

---
+++ src/main/java/me/ssoon/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package me.ssoon.petclinic.model;
+
+public class PetType {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/me/ssoon/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package me.ssoon.petclinic.model;
+
+public class Vet extends Person {
+
+}
